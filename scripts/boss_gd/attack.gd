3cf62b3526cd2d6ca47f8e3c5ef082ae0f2872ec
Yago <yagomauricio.lima@gmail.com>
2025-11-22 20:07:15 -0400

    hitbox do boss ativando no frame de ataque

--- scripts/boss_gd/attack.gd
+++ scripts/boss_gd/attack.gd
@@ -2,22 +2,34 @@ extends State
 
 var can_transition: bool = false
 @onready var hitbox: Area2D = $"../../hitbox"
+var hitbox_active: bool = false
+var already_hit: bool = false
+
 
 func enter():
 	super.enter()
 	combo()
 
+
+func enable_hitbox():
+	hitbox_active = true
+	already_hit = false  # reset hit for each attack
+	hitbox.set_deferred("monitoring", true)
+	hitbox.set_deferred("monitorable", true)
+	hitbox.get_node("CollisionShape2D").set_deferred("disabled", false)
+
+func disable_hitbox():
+	hitbox_active = false
+	hitbox.set_deferred("monitoring", false)
+	hitbox.set_deferred("monitorable", false)
+	hitbox.get_node("CollisionShape2D").set_deferred("disabled", true)
+
+
+
 func attack(move):
 	animation_player.play("attack" + move)
-	var overlapping_areas = hitbox.get_overlapping_areas()
+	await animation_player.animation_finished  # still needed
 
-	for area in overlapping_areas:
-		if area.name=="hurtbox":
-			var parent = area.get_owner()
-			if parent.name=="player":
-				parent.take_damage(10)
-
-	await animation_player.animation_finished
 
 
 func combo():
@@ -38,3 +50,14 @@ func transition():
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name=="attack2":
 		combo()
+
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	if not hitbox_active:
+		return  # ignore hits when not on valid attack frames
+
+	if area.name == "hurtbox" and area.get_owner() == player:
+
+			if not already_hit:
+				player.take_damage(10)
+				already_hit = true
