bf5eff3cb38034face1b627e504f8b2f41f253c2
marceru <mcfilho2002@gmail.com>
2025-11-23 14:34:10 -0300

    arrumei a tela e a neve

--- mundo/Scenes/LevelBase/SnowParticles.gd
+++ mundo/Scenes/LevelBase/SnowParticles.gd
@@ -2,8 +2,13 @@ extends GPUParticles2D
 
 class_name SnowParticles
 
-
-
 @export var emission_offset : float = 200.0
+var player
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready():
+	player = get_tree().get_first_node_in_group("player")
+
+func _process(delta: float) -> void:
+	if player:
+		global_position.x = player.global_position.x
+		global_position.y = player.global_position.y - emission_offset
